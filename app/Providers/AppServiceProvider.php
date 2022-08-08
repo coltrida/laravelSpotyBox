7f9cac14408f72d05752a910a719b7de482596d0
coltrida <coltrida@gmail.com>
2022-08-08 16:57:53 +0200

    inserita forzatura a https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
         \Schema::defaultStringLength(191);
+        if (config('app.env') == 'production'){
+            \URL::forceScheme('https');
+        }
+
     }
 }
